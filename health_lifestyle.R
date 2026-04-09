8caf22691c77b128fe9a2b568b8d447de5bd9303
Eveline Breton <evelinebreton@hotmail.com>
2026-04-08 20:16:49 -0400

    Analyse hétéroscédasticité

--- health_lifestyle.R
+++ health_lifestyle.R
@@ -189,6 +189,7 @@ health_lifestyle_ds04 %>%
   theme_minimal()
 
 
+
 #---------------------------------------------------------------------Tableau 1
 #Version 1 trouvées dans Copilot
 
@@ -418,10 +419,52 @@ which(abs(dff3) > seuil_dff3)
 
 
 #----------------------------Homoscédasticité
+#modèle complet
+valeurs_aj3=fitted(mod3_mult)
+residus3 <- rstandard(mod3_mult)
+plot(valeurs_aj3,residus3)
 
-ychapeau3=fitted(mod3_mult)
-r3 <- rstandard(mod3_mult)
-plot(ychapeau3, r3)
+
+#étant donné ligne de points, graphique avec chacune des variables pour identifier
+#variables fautive
+#modèle 1 (simple)
+valeurs_aj1=fitted(mod1_simple)
+residus1 <- rstandard(mod1_simple)
+plot(valeurs_aj1,residus1)
+
+
+plot(mod1_simple)
+
+#modèle avec âge seulement
+mod_age<- lm(Health_Score ~ Age , data = health_lifestyle_ds04)
+valeurs_aj_age=fitted(mod_age)
+residus_age <- rstandard(mod_age)
+plot(valeurs_aj_age,residus_age)
+
+#la ligne est probablement dûe à la variable dépendante indice de santé?
+
+
+
+#avec données avant nettoyage?
+mod3_mult_ds2<- lm(Health_Score ~ Sleep_Hours+Age
+               +Exercise_Frequency
+               +Diet_Quality
+               +Smoking_Status
+               +Alcohol_Consumption
+               , data = health_lifestyle_ds02)
+
+valeurs_aj3_ds2=fitted(mod3_mult_ds2)
+residus3_ds2 <- rstandard(mod3_mult_ds2)
+plot(valeurs_aj3_ds2,residus3_ds2)
+#résultat: même chose
+
+
+
+#modèle complet
+
+valeurs_aj3=fitted(mod3_mult)
+residus3 <- rstandard(mod3_mult)
+plot(valeurs_aj3,residus3)
 
 #graphique bizarre, sandwich?
 plot(mod3_mult)
@@ -430,6 +473,7 @@ plot(mod3_mult)
 library(sandwich); library(lmtest); library(car)
 coeftest(mod3_mult,vcov=vcovHC(mod3_mult,type = "HC3"))                                         
 Confint(mod3_mult,vcov.=vcovHC(mod3_mult,type="HC3"))
+
 
 #analyse de résidus, diagnostique, analyse de sensibilité si variable influente
 
@@ -477,5 +521,225 @@ legend("bottomright",
        lwd = 2,
        bty = "n")
 
+#----------------------------------------Multicolinéarité
+library(car)
+vif(mod3_mult)
+
+###############################################################################
+#----------------------------------------------------------------------------
+#Health score: variable dépendante distribution atypique avec énorme bande à 100%
+#------------------------------------------------------------------------------
+#combien d'individus ont 100?
+sum(health_lifestyle_ds03$Health_Score == 100, na.rm = TRUE)
+#résultat: 242
+#si on enlève ces 242 de l'échantillon et qu'on fait tous les tests et analyses?
+#-------------------------------------------------------------------------------
+
+#enlever de la base de données ces valeurs aberrantes
+#Renaming the dataset before deleting rows
+health_lifestyle_ds05 <- health_lifestyle_ds03
+
+
+#formule identiée avec Copilot
+health_lifestyle_ds05 <- health_lifestyle_ds05 %>%
+  filter(!(Diet_Quality > 100 | Alcohol_Consumption < 0| Age<18 | Health_Score ==100))
+
+sum(health_lifestyle_ds05$Diet_Quality > 100
+    | health_lifestyle_ds05$Alcohol_Consumption < 0
+    | health_lifestyle_ds05$Health_Score ==100
+    | health_lifestyle_ds05$Alcohol_Consumption <18, na.rm = TRUE)
+
+
+#combien de lignes au jeu de données maintenant?
+nrow(health_lifestyle_ds05)
+
+summary(health_lifestyle_ds05)
+
+#Revisualiser les données
+vars <- c("Age", "BMI", "Diet_Quality", "Sleep_Hours",
+          "Alcohol_Consumption", "Health_Score")
+
+par(mfrow = c(2, 3))  
+
+for (v in vars) {
+  boxplot(health_lifestyle_ds05[[v]], main = v)
+}
+
+
+
+health_lifestyle_ds05 %>%
+  select(where(is.numeric)) %>%
+  pivot_longer(everything()) %>%
+  ggplot(aes(x = value)) +
+  geom_histogram(bins = 30, fill = "steelblue", color = "white") +
+  facet_wrap(~ name, scales = "free") +
+  theme_minimal()
+
+
+
+#---------------------------------------------------------------------Tableau 1
+#à faire selon version adoptée plus  haut
+
+#------------------------------------------------------------------------------
+#--------------------------------------------------------------ANALYSE
+#-----------------------------------------------------------------------------
+
+#------------------------------MODELE------------------------------------------
+#########tester linéarité du modèle (cours 8)
+#Faire un graphique de nuage de points entre x et y
+# Nuage de points
+par(mfrow = c(1, 1))
+
+plot(health_lifestyle_ds05$Sleep_Hours,health_lifestyle_ds05$Health_Score,
+     xlab = "Heures de sommeil",
+     ylab = "Indice de santé",
+     main= "Linéarité",
+     pch = 16, col = "grey40")
+
+
+
+#---------------------------------Régression
+#Question de recherche: Quelle est l’association entre l’indice santé et les heures de sommeil?
+#Modèle de régression simple
+mod1b_simple <- lm(Health_Score ~ Sleep_Hours, data = health_lifestyle_ds05)
+abline(mod1_simple,col = "red", lwd = 2)
+summary(mod1_simple)
+confint(mod1_simple)
+
+
+
+#modèle de régression multiple avec toutes les variables de confusion
+mod3b_mult<- lm(Health_Score ~ Sleep_Hours+Age
+               +Exercise_Frequency
+               +Diet_Quality
+               +Smoking_Status
+               +Alcohol_Consumption
+               , data = health_lifestyle_ds05)
+summary(mod3b_mult)
+confint(mod3b_mult)
+
+
+#Modèle avec âge come modificateur d'effet
+mod4b_mult<- lm(Health_Score ~ Sleep_Hours*Age
+               +Exercise_Frequency
+               +Diet_Quality
+               +Smoking_Status
+               +Alcohol_Consumption
+               , data = health_lifestyle_ds05)
+summary(mod4b_mult)
+confint(mod4b_mult)
+
+#----------------------------DIAGNOSTICS--------------------------------------
+
+#----------Mesures d'influence
+#????Est-ce qu'on fait les analyses de résidus sur le modèle simple ou complet?
+
+
+#Distance de cook
+cook1b <- cooks.distance(mod1b_simple)
+which(cook1 > 1)
+which(cook1 > 0.5)
+cook1b
+
+cook3b <- cooks.distance(mod3b_mult)
+which(cook3b > 1)
+which(cook3b > 0.5)
+cook3b
+
+#DFBETAS
+dfb1b <- dfbetas(mod1b_simple) 
+drapeau1b <- apply(abs(dfb1b) > 1, 1, any)
+which(drapeau1b)
+
+dfb1b[drapeau1b, ]
+coef(mod1b_simple)
+
+dfb3b <- dfbetas(mod3b_mult) 
+drapeau3b <- apply(abs(dfb3b) > 1, 1, any)
+which(drapeau3b)
+
+dfb3b[drapeau3b, ]
+coef(mod3b_mult)
+
+#DFFITS
+dff1 <- dffits(mod1b_simple)
+nb<-692
+pp1b<-2
+seuil_dff1b <- 3 * sqrt(pp1b / (nb - pp1b))
+which(abs(dff1) > seuil_dff1)
+
+dff3b <- dffits(mod3b_mult)
+nb<-692
+pp3b<-12
+seuil_dff3b <- 3 * sqrt(pp3b / (nb - pp3b))
+which(abs(dff3b) > seuil_dff3b)
+
+
+#----------------------------Homoscédasticité
+#modèle complet
+valeurs_aj3b=fitted(mod3b_mult)
+residus3b <- rstandard(mod3b_mult)
+plot(valeurs_aj3b,residus3b)
+
+
+
+#graphique bizarre, sandwich?
+plot(mod3b_mult)
+
+
+library(sandwich); library(lmtest); library(car)
+coeftest(mod3b_mult,vcov=vcovHC(mod3b_mult,type = "HC3"))                                         
+Confint(mod3b_mult,vcov.=vcovHC(mod3b_mult,type="HC3"))
+
+
+#analyse de résidus, diagnostique, analyse de sensibilité si variable influente
+
+
+#---------------------------Linéarité
+
+plot(health_lifestyle_ds05$Sleep_Hours,health_lifestyle_ds05$Health_Score,
+     xlab = "Heures de sommeil",
+     ylab = "Indice de santé",
+     main= "Linéarité",
+     pch = 16, col = "grey40")
+
+
+mod.linb <- lm(Health_Score ~ Sleep_Hours, data = health_lifestyle_ds05)
+
+mod.quadb <- lm(Health_Score ~ Sleep_Hours + I(Sleep_Hours^2), data = health_lifestyle_ds05)
+
+library(splines)
+mod.nsb <- lm(Health_Score ~ ns(Sleep_Hours, df = 4), data = health_lifestyle_ds05)
+
+
+
+## 3) Grille de x pour tracer les relations estimées
+xgb <- seq(min(health_lifestyle_ds05$Sleep_Hours), max(health_lifestyle_ds05$Sleep_Hours), length.out = 300)
+newdatb <- data.frame( Sleep_Hours= xgb)
+
+pred.linb  <- predict(mod.linb,  newdata = newdatb)
+pred.quadb <- predict(mod.quadb, newdata = newdatb)
+pred.nsb   <- predict(mod.nsb,   newdata = newdatb)
+
+## 4) Graphique : données + 3 courbes estimées
+plot(health_lifestyle_ds05$Sleep_Hours,health_lifestyle_ds05$Health_Score, pch = 16, cex = 0.7,
+     xlab = "Heures de sommeil", ylab = "Indice de santé",
+     main = "Relations estimées")
+
+lines(xgb, pred.linb,  lwd = 2, lty = 1)
+lines(xgb, pred.quadb, lwd = 2, lty = 2, col = 'blue')
+lines(xgb, pred.nsb,   lwd = 2, lty = 3, col = 'red')
+
+legend("bottomright",
+       legend = c("Modèle linéaire",
+                  "Modèle quadratique",
+                  "Spline naturelle (df = 4)"),
+       lty = c(1, 2, 3),
+       lwd = 2,
+       bty = "n")
+
+#----------------------------------------Multicolinéarité
+library(car)
+vif(mod3b_mult)
 
 
